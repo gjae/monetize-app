5a792bc7e06f6c629fc18c328cf7cbda7d46ce84
Giovanny Avila <gjavila1995@gmail.com>
2017-11-29 00:37:21 +0100

    Configuracion de credenciales de facebook

---
+++ app/config/socials.js
@@ -0,0 +1,8 @@
+const socials_config = {
+	facebook: {
+		clientID: "300840307073154",
+		clientSecret: "e63b3a13ff680ce8af0ca314dd224fe2",
+		callbackURL: "http://localhost:8000/auth/facebook",
+		profileFields: [ 'id', 'name', 'displayName', 'picture', 'email' ],
+	}
+}
